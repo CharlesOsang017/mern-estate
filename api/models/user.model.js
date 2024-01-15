97e8b0d270367f01f4401137bdb63075f277def2
CharlesOsang017 <charlesosango02@gmail.com>
2024-01-15 15:01:21 +0300

    create and add OAuth component

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,6 +16,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    avatar: {
+      type: String,
+      default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&psig=AOvVaw0aY0t8gyqSa5UTzPH4vDz4&ust=1705405003593000&source=images&cd=vfe&ved=0CBMQjRxqFwoTCMDU7_Cm34MDFQAAAAAdAAAAABAE"
+    }
   },
   { timestamps: true }
 );
---
+++ client/src/components/OAuth.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { app } from '../firebase'
+import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth'
+import { useDispatch } from 'react-redux'
+import { signInSuccess } from '../redux/user/userSlice'
+import {useNavigate} from 'react-router-dom'
+
+const OAuth = () => {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const handleGoogleAuth = async ()=>{
+        try {
+            const provider = new GoogleAuthProvider()
+            const auth = getAuth(app)
+            const result = await signInWithPopup(auth, provider)
+            // console.log(result)
+            const res = await fetch('api/auth/google', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({name: result.user.displayName, email: result.user.email, photo:result.user.photoURL}),
+            })
+            const data = await res.json()
+            dispatch(signInSuccess(data))
+            navigate('/')
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+  return (
+   <button type='button' onClick={handleGoogleAuth} className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95">continue with google</button>
+  )
+}
+
+export default OAuth
