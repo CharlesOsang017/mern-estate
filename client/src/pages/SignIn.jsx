3d0a0debddfac3c2ff48ecc15c119c698ea8a933
CharlesOsang017 <charlesosango02@gmail.com>
2024-01-15 11:22:08 +0300

    Add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 // import toast from 'react-hot-toast';
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInFailure,
+  signInStart,
+  signInSuccess,
+} from "../redux/user/userSlice";
 
 const SignIn = () => {
   const [formData, setFormData] = useState({});
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const navigate = useNavigate()
+  const {loading, error} = useSelector((state)=> state.user)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -17,37 +23,33 @@ const SignIn = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    try {      
-    setLoading(true)
-    const res = await fetch("/api/auth/signin", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
-    const data = await res.json();
-    if(data.success === false){
-        setLoading(false)
+    try {
+      // setLoading(true)
+      dispatch(signInStart());
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setLoading(false);
         setError(data.message);
         return;
-    }
-    setLoading(false)
-    // toast.success('Logged in successfully')
-    setError(null)
-    navigate('/')
-
+      }
+      dispatch(signInSuccess(data));
+      navigate("/");
     } catch (error) {
-      setLoading(false)
-    //   toast.error(error.message)
-      setError(error.message)
-    }    
+      dispatch(signInFailure(data.message));
+    }
   };
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-       <input
+        <input
           type='email'
           placeholder='email'
           className='border p-3 rounded-lg'
@@ -61,8 +63,11 @@ const SignIn = () => {
           id='password'
           onChange={handleChange}
         />
-        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
-          {loading ? 'Loading...' : 'Sign In'}
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? "Loading..." : "Sign In"}
         </button>
       </form>
       <div className='flex gap-2 mt-5'>
