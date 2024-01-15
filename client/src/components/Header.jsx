89dab78eba0f43e827b8b3be8cfae0f623378fa3
CharlesOsang017 <charlesosango02@gmail.com>
2024-01-15 19:33:03 +0300

    complete profile UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-  const {currentUser} = useSelector(state => state.user)
+  const {currentUser} = useSelector((state) => state.user)
   // console.log(currentUser)
   return (
     <header className='bg-slate-200 shadow-md'>
