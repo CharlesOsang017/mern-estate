6a14272f99076c943e331dc47ee0387bc898c811
CharlesOsang017 <charlesosango02@gmail.com>
2024-01-16 09:27:49 +0300

    user update route api

--- api/index.js
+++ api/index.js
@@ -2,12 +2,16 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 //initializing express app
 const app = express();
+
 app.use(express.json());
+app.use(cookieParser())
+
 app.listen(3000, () => {
   console.log(`Server is running on port 3000`);
 });
