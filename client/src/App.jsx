5b36870aaebc0de35a9ae058cca23992dc0aeb4c
CharlesOsang017 <charlesosango02@gmail.com>
2024-01-22 17:30:45 +0300

    Fixed default profile image

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,37 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
-import About from "./pages/About"
-import Profile from "./pages/Profile"
-import Header from "./components/Header"
-import { Toaster } from 'react-hot-toast';
-import PrivateRoute from "./components/PrivateRoute"
-import CreateListing from "./pages/CreateListing"
-import UpdateListing from "./pages/UpdateListing"
-import Listing from "./pages/Listing"
-import Search from "./pages/Search"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import { Toaster } from "react-hot-toast";
+import PrivateRoute from "./components/PrivateRoute";
+import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
+import Listing from "./pages/Listing";
+import Search from "./pages/Search";
+import { useSelector } from "react-redux";
 const App = () => {
+  const { currentUser } = useSelector((state) => state.user);
   return (
-  <BrowserRouter>
-  <Header />
-  <Toaster />
-  <Routes>
-    <Route path='/' element={<Home />}/>
-    <Route path='/search' element={<Search />}/>
-    <Route path='/sign-in' element={<SignIn />}/>
-    <Route path='/sign-up' element={<SignUp />}/>
-    <Route path='/about' element={<About />}/>
-    <Route exact path='/listing/:listingId' element={<Listing />}/>
-    <Route  element={<PrivateRoute />}>
-    <Route path='/profile' element={<Profile />}/>
-    <Route path='/create-listing' element={<CreateListing />}/>
-    <Route path='/update-listing/:listingId' element={<UpdateListing />}/>
-    </Route>
-  </Routes>
-  </BrowserRouter>
-  )
-}
-export default App
+    <BrowserRouter>
+      <Header />
+      <Toaster />
+      <Routes>
+        <Route element={<PrivateRoute />}>
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/create-listing' element={<CreateListing />} />
+        <Route path='/update-listing/:listingId' element={<UpdateListing />} />
+        </Route>
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/about' element={<About />} />
+        <Route  path='/listing/:listingId' element={<Listing />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+export default App;
